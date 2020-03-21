d37c4d93be7f76ce8629d5c59b514e4a528a89ac
Phil Sun <psun.04@gmail.com>
2020-03-20 23:36:17 -0500

    added some color to the notes and added a button for each audio file

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,72 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                print('Playing audio file...');
-                final player = AudioCache();
-                player.play('note2.wav');
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  print('Playing audio file...');
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+                child: Text('Note 1'),
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  print('Playing audio file...');
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                },
+                child: Text('Note 2'),
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  print('Playing audio file...');
+                  final player = AudioCache();
+                  player.play('note3.wav');
+                },
+                child: Text('Note 3'),
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  print('Playing audio file...');
+                  final player = AudioCache();
+                  player.play('note4.wav');
+                },
+                child: Text('Note 4'),
+              ),
+              FlatButton(
+                color: Colors.teal,
+                onPressed: () {
+                  print('Playing audio file...');
+                  final player = AudioCache();
+                  player.play('note5.wav');
+                },
+                child: Text('Note 5'),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  print('Playing audio file...');
+                  final player = AudioCache();
+                  player.play('note6.wav');
+                },
+                child: Text('Note 6'),
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  print('Playing audio file...');
+                  final player = AudioCache();
+                  player.play('note7.wav');
+                },
+                child: Text('Note 7'),
+              ),
+            ],
           ),
         ),
       ),
